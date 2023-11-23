ebe268d35b9bb05f770f815ed4d8d4bcd250deb6
tejasmk-tkp <tejasmk.tkp@gmail.com>
2023-11-23 14:19:56 +0530

    Completed State calculation for one time-step

--- Autonomous_Navigation/ekf.cpp
+++ Autonomous_Navigation/ekf.cpp
@@ -30,6 +30,54 @@ Eigen::Matrix3d P(Eigen::Matrix3d& A_t_minus_1, Eigen::Matrix3d P_t_minus_1, Eig
 
 }
 
+Eigen::Vector3d Y(Eigen::Matrix3d H_t, Eigen::Vector3d X_t, Eigen::Vector3d Sensor_error) {
+
+	Eigen::Vector3d Y_t = (H_t * X_t) + Sensor_error; //Predicted Future state estimate
+
+	return Y_t;
+
+}
+
+Eigen::Vector3d Y_C(Eigen::Vector3d X_t, Eigen::Vector3d Y_t) {
+
+	Eigen::Vector3d Y_t_C = X_t - Y_t;
+
+	return Y_t_C;
+
+}
+
+Eigen::Matrix3d S(Eigen::Matrix3d H_t, Eigen::Matrix3d P_t, Eigen::Matrix3d R_t) {
+
+	Eigen::Matrix3d S_t = (H_t * P_t * H_t.transpose() + R_t);
+
+	return S_t;
+
+}
+
+Eigen::Matrix3d K(Eigen::Matrix3d P_t, Eigen::Matrix3d H_t, Eigen::Matrix3d S_t) {
+
+	Eigen::Matrix3d K_t = P_t * H_t.transpose() * S_t.inverse();
+
+	return K_t;
+
+}
+
+Eigen::Vector3d X_C(Eigen::Vector3d X_t, Eigen::Matrix3d K_t, Eigen::Vector3d Y_t_C) {
+
+	Eigen::Vector3d X_t_C = X_t + (K_t * Y_t_C);
+
+	return X_t_C;
+
+}
+
+Eigen::Matrix3d P_C(Eigen::Matrix3d K_t, Eigen::Matrix3d H_t, Eigen::Matrix3d P_t) {
+
+	Eigen::Matrix3d P_t_C = (Eigen::Matrix3d::Identity() - (K_t * H_t)) * P_t;
+
+	return P_t_C;
+
+}
+
 int main() {
 
 	//Step 1: Predicted State Estimate
@@ -86,7 +134,71 @@ int main() {
 	
 
 
-	Eigen::Vector2d Y_t; //Predicted Future State
+	Eigen::Matrix3d H_t; //measurement matrix (used to convert the state at time t into predicted sensor observations at time t) that has n rows and 3 columns (i.e. a column for each state variable).
 	
+	H_t << 1, 0, 0, 0, 1, 0, 0, 0, 1; //Assuming sensors directly give state of the rover
+	
+	Eigen::Vector3d Sensor_error; //Error of Sensor data
+	
+	Sensor_error << 0.02, 0.02, 0.02; //Assumed
+	
+	Eigen::Vector3d Y_t = Y(H_t, X_t, Sensor_error);
+
+	std::cout << Y_t << std::endl; //Print Y_t
+
+
+
+	//Step 4: Measurement Residual
+	
+
+
+	Eigen::Vector3d Y_t_C = Y_C(X_t, Y_t);
+
+	std::cout << Y_t_C << std::endl;
+
+
+
+	//Step 5: Residual Covariance
+	
+
+	
+	Eigen::Matrix3d R_t; //sensor measurement noise covariance matrix…which is a covariance matrix that has the same number of rows as sensor measurements and same number of columns as sensor measurements
+
+	R_t << 1, 0, 0, 0, 1, 0, 0, 0, 1; //Tweak by trail and error
+
+	Eigen::Matrix3d S_t = S(H_t, P_t, R_t);
+
+	std::cout << S_t << std::endl;
+	
+
+
+	//Step 6: Near Optimal Kalman Gain
+	
+
+	Eigen::Matrix3d K_t = K(P_t, H_t, S_t);
+
+	std::cout << K_t << std::endl;
+
+
+
+	//Step 7: Updated State Estimate
+	
+
+
+	Eigen::Vector3d X_t_C = X_C(X_t, K_t, Y_t_C);
+
+	std::cout << X_t_C << std::endl;
+
+
+
+	//Step 8: Updated Covariance of State
+	
+
+
+	Eigen::Matrix3d P_t_C = P_C(K_t, H_t, P_t);
+
+	std::cout << P_t_C << std::endl;
+
 	return 0;
+
 }
